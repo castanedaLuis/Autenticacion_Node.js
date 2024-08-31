d13c3fbb68fe9cf510172e832c087e76b9fc85e4
Jose Luis Castañeda <programador1996@outlook.es>
2024-08-31 10:56:01 -0600

    feat: :sparkles: Create function logout
    Create function logout

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cookieParser())
 
+//creamos un mildeware
+app.use((req,res, next) =>{
+    const token = req.cookies.access_token
+    req.session = { user : null}
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY)
+        req.session.user = data
+    } catch (error) {}
+
+    //seguir a la sig ruta o mildleware
+    next()
+})
+
 app.get('/', (req, res)=>{
-    res.render('example',{name:"José Luis Castañeda"})
+    const { user } = req.session
+    res.render('index',{name: user})   
 })
 
 app.post('/login', async (req,res)=>{
@@ -27,7 +41,7 @@ app.post('/login', async (req,res)=>{
         )
 
         res
-        .cookie('acces_token',token , {
+        .cookie('access_token',token , {
             httpOnly:true, //la cookie solo puede ser accedida por el servidor
             secure: process.env.NODE_ENV === 'production', // la cookie solo se puede acceder por https
             sameSite: 'strict', // Que solo se pueda accerder desde el mismo dominio
@@ -52,9 +66,20 @@ app.post('/register',(req,res)=>{
 
 })
 
-app.post('/logout',(req,res)=>{})
+app.post('/logout',(req,res)=>{
+    const { user } = req.session
+    res
+    .clearCookie('access_token')
+    .render('index',user)
+})
 
-app.post('/protected',(req,res)=>{})
+app.post('/protected',(req,res)=>{
+    const { user } = req.session
+    if(!user){
+        res.status(403).send('Access not authorized')
+    }
+    res.render('protected', user)
+})
 
 
 
